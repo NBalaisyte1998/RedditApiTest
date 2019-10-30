c8b19019c9d2c551c7ef586ed5ccff7f1e211380
unknown <nbalaisyte@spartaglobal.com>
2019-10-30 22:19:11 +0200

    kind test

--- RedditAPITest/RedditAPI_Service/RedditService.cs
+++ RedditAPITest/RedditAPI_Service/RedditService.cs
@@ -3,10 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RedditAPITest.RedditAPI_Service.Data_Handling;
+using RedditAPITest.RedditAPI_Service.HTTP_Manager;
+using Newtonsoft.Json.Linq;
 
 namespace RedditAPITest.RedditAPI_Service
 {
     class RedditService
     {
+        // We need DTO
+        public RedditDTO redditDTO = new RedditDTO();
+        // We need Call Manager
+        public RedditCallManager redditCallManager = new RedditCallManager();
+        // We need JObject
+        public JObject RedditJSON;
+        public RedditService()
+        {
+            // Deserialize the response from the call manager
+            redditDTO.DeserializeNeuroNews(redditCallManager.GetNeuroNews());
+            RedditJSON = JObject.Parse(redditCallManager.GetNeuroNews());
+        }
     }
 }
---
+++ RedditAPITest/Test/RedditTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using RedditAPITest.RedditAPI_Service;
+
+namespace RedditAPITest.Test
+{
+    [TestFixture]
+    class RedditTests
+    {
+        RedditService redditService = new RedditService();
+
+        public RedditTests()
+        {
+        }
+        [Test]
+        public void KindCheck()
+        {
+            Assert.AreEqual("Listing", redditService.redditDTO.redditApiRoot.kind);
+        }
+    }
+}
